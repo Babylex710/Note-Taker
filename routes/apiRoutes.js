3c13ef9603309ef41ead52d5d69c4d1014bcb1af
Lexi M <104409081+Babylex710@users.noreply.github.com>
2022-11-12 13:44:41 -0800

    created html routes.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,33 @@
+// Dependencies
 const router = require('express').Router();
 const fs = require('fs');
+const path = require('path');
+const notes = require('../db/db.json');
 
+// GET request
 router.get('/notes', (req, res) => {
-    const notes = require('./db/db.json');
 
     res.status(200).json(notes);
-})
+});
 
+// POST request
 router.post('/notes', (req, res) => {
-    const notes = ('./db/db.json');
 
-    const { noteTitle, noteText } = req.body;
-    console.log(noteText, noteTitle);
-
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        if(err) {
+    const notesData = notes || [];
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        if (err) {
             res.status(500).end();
-        }else {
-            res.status(200).end();
         }
+        notesData.push(req.body);
+
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesData), (err) => {
+            if (err) {
+                res.status(500).end();
+            } else {
+                res.status(200).end();
+            }
+        });
     });
-    res.end();
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+// Dependencies
 const express = require('express');
 const fs = require('fs');
 const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 
+// Parse incoming json data
 app.use(express.json());
+
+// Parse incoming string data
 app.use(express.urlencoded({extended: true}));
 
+// static
 app.use(express.static('public'));
 
-
+// route middleware
 app.use('/api', apiRoutes);
 
+// Listener
 app.listen(3001, () => console.log('App running on http://localhost:3001/'))
